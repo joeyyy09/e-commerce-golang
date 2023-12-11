9395eabc8217e24e3cc93e64856687a957b60c29
Joeyyy09 <imt_2021063@iiitm.ac.in>
2023-12-11 18:28:02 +0530

    Fixed bug in routes

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func userRoutes(incomingRoutes "gi.Engine")
-{
+func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/users/signup", controllers.Signup())
 	incomingRoutes.POST("/users/login", controllers.Login())
-	incomingRoutes.GET("/admin/addproduct",	controllers.ProductViewerAdmin())
-	incomingRoutes.GET("/users/productview",controllers.SearchProduct())
-	incomingRoutes.GET("/users/search",controllers.SearchProductbyQuery())
+	incomingRoutes.GET("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductbyQuery())
 }
