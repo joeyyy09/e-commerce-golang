cef8484c8116bc5394aacf7a4924eea40f5723ff
Joeyyy09 <imt_2021063@iiitm.ac.in>
2023-12-14 17:29:39 +0530

    Add SignUp Controller logic

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -14,6 +14,56 @@ func HashPassword(password string) string {
 func VerifyPassword(userPassword string, givenPassword string) (bool,string) {
 }
 
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	 var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	 defer cancel()
+
+	 var user models.User
+	 if err := c.BindJSON(&user); err != nil {
+		 c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		 return
+	 }
+
+	 validationError:= Validate.Struct(user)
+	 if validationError != nil {	
+		 c.JSON(http.StatusBadRequest, gin.H{"error": validationError.Error()})
+		 return
+	 }
+
+	 count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+	 if err != nil {
+		log.Panic(err)
+		 c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		 return
+	 }
+
+	 if count > 0 {
+		 c.JSON(http.StatusBadRequest, gin.H{"error": "Email already exists"})
+		 return
+	 }
+
+	 count, err = UserCollection.CountDocuments(ctx, bson.M{"Phone ": user.Phone})
+
+	 defer cancel()
+
+	 if err != nil {
+		log.Panic(err)
+		 c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		 return
+	 }
+
+	 if count > 0 {
+		 c.JSON(http.StatusBadRequest, gin.H{"error": "Phone already exists"})
+		 return
+	 }
+
+	 
+
+
+	}
+}
+
 func Login() gin.HandlerFunc {
 }
 
